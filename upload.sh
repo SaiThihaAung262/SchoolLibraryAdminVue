3c8b288dc02dfe61cbf3acc4b7182e597b3c8864
Thiha Aung <tha.buutii26299@gmail.com>
2023-03-08 22:12:25 +0630

    Modfy

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ./dist/
-npm run build
-cd dist
-zip -r assets.zip ./*
-
-ssh -i ~/Documents/pems/xiaohuangshu.pem root@douyou.test "cd /www/wwwroot/admin-douyou.cici02.com && rm -rf *"
-scp -i ~/Documents/pems/xiaohuangshu.pem /home/minghua/Documents/VueProjects/Liquidity-Pool/LP-Admin/dist/assets.zip  root@douyou.test:/www/wwwroot/admin-douyou.cici02.com/
-ssh -i ~/Documents/pems/xiaohuangshu.pem root@douyou.test "cd /www/wwwroot/admin-douyou.cici02.com && unzip assets.zip"
