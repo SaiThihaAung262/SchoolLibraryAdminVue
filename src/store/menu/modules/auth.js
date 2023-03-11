91be2e458a3e0d2f4710afe8634e52dc800ab213
Thiha Aung <tha.buutii26299@gmail.com>
2023-03-11 23:05:08 +0630

    Moidfy

--- src/store/menu/modules/auth.js
+++ src/store/menu/modules/auth.js
@@ -3,44 +3,11 @@ import { checkPermissions } from "@/utils/permissions";
 export const auth = [
   {
     id: 94,
-    name: "Admin",
+    name: "Admin User",
     icon: "fa-solid fa-users",
-    url: "/auth",
-    type: "group",
+    url: "/auth/admin",
+    type: "page",
     permission: true,
     perName: "auth",
-    children: [
-      {
-        id: 95,
-        name: "Admin User",
-        icon: "fa-solid fa-user",
-        type: "page",
-        permission: true,
-        perName: "adminManagement",
-        url: "/auth/admin",
-        children: [
-          {
-            id: 3,
-            name: "list",
-            perName: "",
-          },
-          {
-            id: 1,
-            name: "addto",
-            perName: "",
-          },
-          {
-            id: 2,
-            name: "edit",
-            perName: "",
-          },
-          {
-            id: 4,
-            name: "delete",
-            perName: "",
-          },
-        ],
-      },
-    ],
   },
 ];
